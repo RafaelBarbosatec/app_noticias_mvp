8a84cc353c01e3b49f49c585429b29174e2f1c71
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2022-07-11 20:13:53 -0300

    adiciona appbar

--- lib/view/login/login_page.dart
+++ lib/view/login/login_page.dart
@@ -5,6 +5,8 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Login')),
+    );
   }
 }
